5e54092e8d3e5e5964c0847691fc4a14c3218ee0
Raine Virta <raine.virta@gmail.com>
2019-01-04 00:28:42 +0200

    add a simple test

---
+++ test/index.test.js
@@ -0,0 +1,23 @@
+const npmInstallTo = require('../')
+const util = require('util')
+const tempy = require('tempy')
+const fs = require('fs')
+const readFile = util.promisify(fs.readFile)
+const readJsonFile = (p) => readFile(p, 'utf8').then(JSON.parse)
+
+test('installs a module', async () => {
+  const tempDir = tempy.directory()
+  const result = await npmInstallTo(tempDir, ['treis@2.6.0'])
+
+  expect(result).toEqual(
+    expect.objectContaining({
+      npmOutput: expect.any(String),
+      packages: {
+        'treis@2.6.0': `${tempDir}/node_modules/treis`
+      }
+    })
+  )
+
+  const pkg = await readJsonFile(`${tempDir}/node_modules/treis/package.json`)
+  expect(pkg.name).toBe('treis')
+}, 10000)
